ef64735e1f3b0b110c56e8d36642290dd95faf25
Diastowo <diastowo@gmail.com>
2021-01-04 23:13:50 +0700

    add Procfile

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
